ed63e7552e567c6ee3f6a7d57c16caf18fa515b3
Peter Jiping Xie <peter.jp.xie@gmail.com>
2019-01-11 15:36:36 +1100

    Add files via upload

--- test_selenium_pytest.py
+++ test_selenium_pytest.py
@@ -1,6 +1,7 @@
 '''
-References:
-Install: https://pypi.org/project/selenium/
+Install:
+pip3 install -U pytest
+pip3 install -U selenium
 
 Notes:
 - Exceptions will cause test to fail and terminate just as assert.
@@ -413,24 +414,6 @@ def test_temp():
     
     print('Finishing function')
     assert ret
-
-    
-    
-'''  
-    from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-driver = webdriver.Firefox()
-driver.get("http://somedomain/url_that_delays_loading")
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "myDynamicElement"))
-    )
-finally:
-    driver.quit()
-'''    
         
 # Test without pytest
 def main():        
